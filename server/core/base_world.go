cbf465c42b8ae59136c85cc9e6657fa3bcbe22a0
Jacob Reckhard <jacobreckhard@gmail.com>
2019-01-19 19:27:36 -0700

    fixed all the compile errors

--- server/core/base_world.go
+++ server/core/base_world.go
@@ -4,21 +4,21 @@ import (
     "github.com/jacobrec/petah-royale/server/api"
 )
 
-func StartWorld(gf *GameIF) {
+func StartWorld(gf GameIF) {
 
     g := makeGame(gf)
 
     gf.OnJoin(func(id interface{}) { onJoin(&g, id) })
     gf.OnLeave(func(id interface{}) { onLeave(&g, id) })
-    gf.OnEvent("shoot", func(id interface{}) { onShoot(&g, id) })
-    gf.OnEvent("move", func(id interface{}) { onMove(&g, id) })
+    gf.OnEvent("shoot", func(id interface{}, ev api.Event) { onShoot(&g, id, ev) })
+    gf.OnEvent("move", func(id interface{}, ev api.Event) { onMove(&g, id, ev) })
 }
 
-func makeGame(gf *GameIF) gameObject {
+func makeGame(gf GameIF) gameObject {
     w := world{80, 60, make([]moveable, 0), make([]immoveable, 0)}
     w.walls = append(w.walls, immoveable{0,0,80,1}, immoveable{0,0,1,60}, immoveable{0,59,80,1}, immoveable{79,0,1,60})
 
-    return gameObject{w, gf}
+    return gameObject{w, gf, make(map[interface{}]int, 3), make(map[int]interface{}, 3)}
 }
 
 
@@ -41,33 +41,34 @@ type immoveable struct {
 type world struct {
     width int
     height int
-    players moveable[]
-    walls immovable[]
+    players []moveable
+    walls []immoveable
 }
 
-func (w* world) getPlayerById(id) *moveable {
+func (w* world) getPlayerById(id int) *moveable {
     for _, p := range w.players {
-        if p.id == p {
+        if p.id == id {
             return &p
         }
     }
+    return nil
 }
 
 type gameObject struct {
     w world
-    gf *GameIF
-    connectionToGame[interface{}] int
-    gameToConnection[int] interface{}
+    gf GameIF
+    connectionToGame map[interface{}]int
+    gameToConnection map[int]interface{}
 }
 
 var playerCount int
 
 func onJoin(g *gameObject, id interface{}){
-    pid = playerCount
+    pid := playerCount
     playerCount++
 
-    connectionToGame[id] = pid
-    gameToConnection[pid] = id
+    g.connectionToGame[id] = pid
+    g.gameToConnection[pid] = id
 
     player := moveable{pid, 2, 2, 0.5}
     g.w.players = append(g.w.players, player)
@@ -80,7 +81,7 @@ func onLeave(g *gameObject, id interface{}){
 
 func onMove(g *gameObject, id interface{}, event api.Event){
     move := event.Data.(api.Move)
-    pid := connectionToGame[id]
+    pid := g.connectionToGame[id]
 
     pp := g.w.getPlayerById(pid)
     pp.x = move.X
@@ -97,6 +98,6 @@ func (g* gameObject) sendPlayerMove(player moveable){
     }
 }
 
-func onShoot(gf *gameObject, id interface{}, event api.Shoot){
-    shoot := event.Data.(api.Shoot)
+func onShoot(gf *gameObject, id interface{}, event api.Event){
+    //shoot := event.Data.(api.Shoot)
 }
--- server/main.go
+++ server/main.go
@@ -3,9 +3,11 @@ package main
 import (
     "github.com/jacobrec/petah-royale/server/server"
     "github.com/jacobrec/petah-royale/server/api"
+    "github.com/jacobrec/petah-royale/server/core"
 )
 
 func main() {
     wsgame := server.NewWSgame(api.DefaultEventsAR())
+    core.StartWorld(&wsgame)
     server.Begin(&wsgame, ":8049")
 }
