195308b0bba5f0a0cf8e53f0d15a0248b6bd4ae6
Peter Elliott <pelliott@ualberta.ca>
2019-01-19 18:43:35 -0700

    made wsgame start

--- server/main.go
+++ server/main.go
@@ -1,28 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "reflect"
-    _ "github.com/jacobrec/petah-royale/server/server"
+    "github.com/jacobrec/petah-royale/server/server"
     "github.com/jacobrec/petah-royale/server/api"
 )
 
-type A struct {
-    A int `json:"a"`
-    B int `json:"b"`
-}
-
-type B struct {
-    B int `json:"b"`
-}
-
 func main() {
-    //server.Begin(":8049")
-    ar := api.EmptyAR()
-
-    ar.AddAction("A", reflect.TypeOf(A{}))
-    ar.AddAction("B", reflect.TypeOf(B{}))
-
-    fmt.Println(ar.ToAction([]byte(`{"action": "A", "data": {"a": 5, "b": 6}}`)).Data.(*A))
-    fmt.Println(ar.ToAction([]byte(`{"action": "B", "data": {"b": 6}}`)).Data.(*B))
+    wsgame := server.NewWSgame(api.DefaultEventsAR())
+    server.Begin(wsgame, ":8049")
 }
