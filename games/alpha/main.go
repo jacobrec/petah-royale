6d43e684d823dd33ce4414602a2e163821d2af86
Peter Elliott <pelliott@ualberta.ca>
2019-01-19 22:01:41 -0700

    refactored out base_game

---
+++ games/alpha/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "github.com/jacobrec/petah-royale/server/server"
+    "github.com/jacobrec/petah-royale/server/api"
+    "github.com/jacobrec/petah-royale/games/alpha/alpha"
+    "github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
+func main() {
+    router := gin.Default()
+    router.Use(CORSMiddleware())
+
+    server.CreateGameServer(&router.RouterGroup, func() *server.WSgame {
+        wsgame := server.NewWSgame(api.DefaultEventsAR())
+        alpha.StartWorld(&wsgame)
+        return &wsgame
+    })
+
+    router.Run(":8049")
+}
