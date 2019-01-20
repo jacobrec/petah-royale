6e692f9c8234e4063d0b2c2ee1179655cb0bcdce
Peter Elliott <pelliott@ualberta.ca>
2019-01-19 20:47:01 -0700

    began using api

--- server/main.go
+++ server/main.go
@@ -4,10 +4,17 @@ import (
     "github.com/jacobrec/petah-royale/server/server"
     "github.com/jacobrec/petah-royale/server/api"
     "github.com/jacobrec/petah-royale/server/core"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-    wsgame := server.NewWSgame(api.DefaultEventsAR())
-    core.StartWorld(&wsgame)
-    server.Begin(&wsgame, ":8049")
+    router := gin.Default()
+
+    server.CreateGameServer(&router.RouterGroup, func() *server.WSgame {
+        wsgame := server.NewWSgame(api.DefaultEventsAR())
+        core.StartWorld(&wsgame)
+        return &wsgame
+    })
+
+    router.Run(":8049")
 }
